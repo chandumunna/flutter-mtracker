1b7bb80bec28424afd4d8ddd1b4b0ac46a9575d2
sanjay-rb <sanjaybabu618@gmail.com>
2022-01-29 14:34:05 +0530

    Shwoing ScaffoldMessenger for collection ref

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mtracker/services/database_manager.dart';
 import 'package:mtracker/widget/monthly_stats.dart';
 
 import 'widget/balance.dart';
@@ -10,6 +11,16 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Future.delayed(const Duration(seconds: 1), () {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          behavior: SnackBarBehavior.floating,
+          content: Text(
+            'Fetched data from ${DatabaseManager.TRANSACTION_COLLECTION_NAME}',
+          ),
+        ),
+      );
+    });
     return SafeArea(
       child: Padding(
         padding: const EdgeInsets.all(8.0),
