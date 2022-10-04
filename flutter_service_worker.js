25060d45cf4abc40938e774cbebeb2d844871c10
Sanjay Ramassery Babu <sanjaybabu618@gmail.com>
2022-10-04 10:02:05 +0530

    new release from Sanjay R B

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -19,8 +19,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "1d73fa4edf59d5dcc85a0c909e6d7b63",
-"/": "1d73fa4edf59d5dcc85a0c909e6d7b63",
+"index.html": "8b01e1514ae22fc7006c666555b3a979",
+"/": "8b01e1514ae22fc7006c666555b3a979",
 "main.dart.js": "318e1a95533d8c20f696fd91c94d6bc5",
 "manifest.json": "90915628ec3006d7fbe0031d63470ec0",
 "version.json": "6896eac87c1da38f326d0df83255a95a"
